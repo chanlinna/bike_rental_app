46fd1459ae962a256e4c1deb6b31f934433e5865
chanlinna <chanlinnalin@gmail.com>
2026-04-19 13:25:53 +0700

    inject in main

--- lib/main_common.dart
+++ lib/main_common.dart
@@ -1,34 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart'; 
-import 'package:bike_rental_app/ui/theme/theme.dart';
+import 'package:provider/provider.dart';
 
-import 'package:bike_rental_app/models/station/station.dart';
+import 'ui/theme/theme.dart';
+import 'ui/screens/map/map_screen.dart';
+import 'ui/screens/journey/journey_screen.dart';
+import 'ui/screens/pass/pass_screen.dart';
+import 'ui/screens/search/view_model/search_view_model.dart';
+import 'ui/screens/search/search_screen.dart';
+import 'ui/screens/bike/bike_list_screen.dart';
+import 'package:bike_rental_app/ui/screens/booking/booking_screen.dart';
+import 'models/station/station.dart';
 
-import 'package:bike_rental_app/data/repositories/station_repository.dart';
-import 'package:bike_rental_app/data/repositories/bike_repository.dart';
-import 'package:bike_rental_app/ui/states/map_state.dart';
-import 'package:bike_rental_app/ui/screens/search/view_model/search_view_model.dart';
-import 'package:bike_rental_app/ui/screens/search/search_screen.dart';
-
-import 'package:bike_rental_app/ui/screens/journey/journey_screen.dart';
-import 'package:bike_rental_app/ui/screens/map/map_screen.dart';
-import 'package:bike_rental_app/ui/screens/pass/pass_screen.dart';
-import 'package:bike_rental_app/ui/screens/bike/bike_list_screen.dart';
-
-void mainCommon() {
-  final stationRepo = StationRepository();
-  final bikeRepo = BikeRepository();
-
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context) => MapState(stationRepo, bikeRepo),
-        )
-      ],
-      child: const MyApp(),
-    ),
-  );
+/// Launch app with injected providers
+void mainCommon(List<InheritedProvider> providers) {
+  runApp(MultiProvider(providers: providers, child: const MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -41,10 +26,10 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   int _currentIndex = 0;
 
-  final List<Widget> _pages = [
-    const MapScreen(), 
-    const JourneyScreen(),
-    const PassScreen(),
+  final List<Widget> _pages = const [
+    MapScreen(),
+    JourneyScreen(),
+    PassScreen(),
   ];
 
   @override
@@ -52,43 +37,49 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: appTheme,
+
       onGenerateRoute: (settings) {
         if (settings.name == '/bikes') {
           final station = settings.arguments as Station;
 
           return MaterialPageRoute(
-            builder: (context) => BikeListScreen(station: station),
+            builder: (_) => BikeListScreen(station: station),
           );
         }
 
         if (settings.name == '/search') {
           return MaterialPageRoute(
-            builder: (context) => ChangeNotifierProvider(
+            builder: (_) => ChangeNotifierProvider(
               create: (_) => SearchViewModel(),
               child: const SearchScreen(),
             ),
           );
         }
+
+        if (settings.name == '/booking') {
+          final bikeId = settings.arguments as String;
+
+          return MaterialPageRoute(
+            builder: (context) => BookingScreen(bikeId: bikeId),
+          );
+        }
+
         return null;
       },
+
       home: Scaffold(
-        // IndexedStack keeps the Map state alive when switching tabs
-        body: IndexedStack(
-          index: _currentIndex,
-          children: _pages,
-        ),
+        body: IndexedStack(index: _currentIndex, children: _pages),
+
         bottomNavigationBar: BottomNavigationBar(
           currentIndex: _currentIndex,
           onTap: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
+            setState(() => _currentIndex = index);
           },
           items: const [
             BottomNavigationBarItem(icon: Icon(Icons.map), label: 'Map'),
             BottomNavigationBarItem(icon: Icon(Icons.route), label: 'Journey'),
             BottomNavigationBarItem(
-              icon: Icon(Icons.confirmation_number),
+              icon: Icon(Icons.card_membership),
               label: 'Pass',
             ),
           ],
--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,6 +1,36 @@
+import 'package:provider/provider.dart';
 
-import 'package:bike_rental_app/main_common.dart';
+import 'main_common.dart';
+
+import 'data/repositories/station_repository.dart';
+import 'data/repositories/bike_repository.dart';
+import 'data/repositories/booking/booking_repository.dart';
+import 'data/repositories/booking/booking_repository_firebase.dart';
+
+import 'ui/states/map_state.dart';
+import 'ui/states/booking_state.dart';
+
+List<InheritedProvider> get devProviders {
+  return [
+    Provider<StationRepository>(create: (_) => StationRepository()),
+
+    Provider<BikeRepository>(create: (_) => BikeRepository()),
+
+    Provider<BookingRepository>(create: (_) => BookingRepositoryFirebase()),
+
+    ChangeNotifierProvider<MapState>(
+      create: (context) => MapState(
+        context.read<StationRepository>(),
+        context.read<BikeRepository>(),
+      ),
+    ),
+
+    ChangeNotifierProvider<BookingState>(
+      create: (context) => BookingState(context.read<BookingRepository>()),
+    ),
+  ];
+}
 
 void main() {
-  mainCommon();
+  mainCommon(devProviders);
 }
