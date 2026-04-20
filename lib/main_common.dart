f35adcc3bf0c7e5886a2de0b56fcf32a2d18a5cb
chanlinna <chanlinnalin@gmail.com>
2026-04-20 19:45:25 +0700

    update navigation in main_common

--- lib/main_common.dart
+++ lib/main_common.dart
@@ -8,10 +8,8 @@ import 'ui/screens/pass/pass_screen.dart';
 import 'ui/screens/search/view_model/search_view_model.dart';
 import 'ui/screens/search/search_screen.dart';
 import 'ui/screens/bike/bike_list_screen.dart';
-import 'package:bike_rental_app/ui/screens/booking/booking_screen.dart';
 import 'models/station/station.dart';
 
-/// Launch app with injected providers
 void mainCommon(List<InheritedProvider> providers) {
   runApp(MultiProvider(providers: providers, child: const MyApp()));
 }
@@ -26,11 +24,13 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   int _currentIndex = 0;
 
-  final List<Widget> _pages = const [
-    MapScreen(),
-    JourneyScreen(),
-    PassScreen(),
-  ];
+  void switchTab(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  List<Widget> get _pages => [MapScreen(), JourneyScreen(onGoToMap: () => switchTab(0)), PassScreen()];
 
   @override
   Widget build(BuildContext context) {
@@ -56,14 +56,6 @@ class _MyAppState extends State<MyApp> {
           );
         }
 
-        if (settings.name == '/booking') {
-          final bikeId = settings.arguments as String;
-
-          return MaterialPageRoute(
-            builder: (context) => BookingScreen(bikeId: bikeId),
-          );
-        }
-
         return null;
       },
 
@@ -72,9 +64,7 @@ class _MyAppState extends State<MyApp> {
 
         bottomNavigationBar: BottomNavigationBar(
           currentIndex: _currentIndex,
-          onTap: (index) {
-            setState(() => _currentIndex = index);
-          },
+          onTap: switchTab,
           items: const [
             BottomNavigationBarItem(icon: Icon(Icons.map), label: 'Map'),
             BottomNavigationBarItem(icon: Icon(Icons.route), label: 'Journey'),
