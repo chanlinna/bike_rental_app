54b448d5bd9c58e4085bfe549dc39cc81421a486
In Chanaliza <inchanaliza@gmail.com>
2026-04-20 21:02:51 +0700

    update(map) with my location pin

--- lib/ui/screens/map/widgets/bike_map_widget.dart
+++ lib/ui/screens/map/widgets/bike_map_widget.dart
@@ -26,22 +26,48 @@ class BikeMapWidget extends StatelessWidget {
         mapVM.onMapCreated(controller);
       },
       initialCameraPosition: const CameraPosition(
-        target: LatLng(11.5564, 104.9282),
+        target: LatLng(11.5564, 104.9282), 
         zoom: 13,
       ),
-      markers: stations.map((station) {
-        return Marker(
-          markerId: MarkerId(station.stationId),
-          position: LatLng(station.latitude, station.longitude),
-          icon: BitmapDescriptor.defaultMarkerWithHue(
-            station.bikeCount > 0
-                ? BitmapDescriptor.hueGreen
-                : BitmapDescriptor.hueRed,
-          ),
+      markers: {
+        ...stations.map((station) {
+          return Marker(
+            markerId: MarkerId(station.stationId),
+            position: LatLng(station.latitude, station.longitude),
+            icon: BitmapDescriptor.defaultMarkerWithHue(
+              station.bikeCount > 0
+                  ? BitmapDescriptor.hueGreen
+                  : BitmapDescriptor.hueRed,
+            ),
+            onTap: () => mapVM.selectStation(station),
+          );
+        }),
 
-          onTap: () => mapVM.selectStation(station),
-        );
-      }).toSet(),
+        if (mapVM.currentLocation != null)
+          Marker(
+            markerId: const MarkerId('user_label'),
+            position: mapVM.currentLocation!,
+            icon: BitmapDescriptor.defaultMarkerWithHue(
+              BitmapDescriptor.hueAzure,
+            ),
+            infoWindow: const InfoWindow(
+              title: "My Location",
+              snippet: "Starting Point",
+            ),
+          ),
+      },
+      circles: {
+        if (mapVM.currentLocation != null)
+          Circle(
+            circleId: const CircleId('user_location_circle'),
+            center: mapVM.currentLocation!,
+            radius: 50, 
+            fillColor: Colors.blue.withOpacity(0.3),
+            strokeColor: Colors.blueAccent,
+            strokeWidth: 2,
+            zIndex: 10,
+          ),
+      },
     );
   }
 
