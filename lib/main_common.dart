43870a3680185fbea23ce12d406aab2c5209ab80
Neitong <Hengneitong.keo@student.cadt.edu.kh>
2026-04-20 15:11:32 +0700

    Injecting to the main

--- lib/main_common.dart
+++ lib/main_common.dart
@@ -1,10 +1,14 @@
 import 'package:bike_rental_app/ui/screens/journey/journey_screen.dart';
 import 'package:bike_rental_app/ui/screens/map/map_screen.dart';
 import 'package:bike_rental_app/ui/screens/pass/pass_screen.dart';
+import 'package:bike_rental_app/ui/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void mainCommon() {
-  runApp(const MyApp());
+void mainCommon(List<InheritedProvider> providers) {
+  runApp(
+    MultiProvider(providers: providers, child: const MyApp()),
+  );
 }
 
 class MyApp extends StatefulWidget {
@@ -18,8 +22,8 @@ class _MyAppState extends State<MyApp> {
   int _currentIndex = 0;
 
   final List<Widget> _pages = const [
-    MapScreen(),
-    JourneyScreen(),
+    const MapScreen(),
+    const JourneyScreen(),
     PassScreen(),
   ];
 
@@ -27,22 +31,31 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: appTheme,
+
+      onGenerateRoute: (settings) {
+        if (settings.name == '/subscriptions') {
+          return MaterialPageRoute(
+            builder: (context) => const PassScreen(),
+          );
+        }
+
+        return null;
+      },
 
       home: Scaffold(
-        body: _pages[_currentIndex],
+        body: IndexedStack(index: _currentIndex, children: _pages),
 
         bottomNavigationBar: BottomNavigationBar(
           currentIndex: _currentIndex,
           onTap: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
+            setState(() => _currentIndex = index);
           },
           items: const [
             BottomNavigationBarItem(icon: Icon(Icons.map), label: 'Map'),
             BottomNavigationBarItem(icon: Icon(Icons.route), label: 'Journey'),
             BottomNavigationBarItem(
-              icon: Icon(Icons.confirmation_number),
+              icon: Icon(Icons.card_membership),
               label: 'Pass',
             ),
           ],
--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,6 +1,18 @@
 
 import 'package:bike_rental_app/main_common.dart';
+import 'package:bike_rental_app/data/repositories/pass/pass_repository.dart';
+import 'package:bike_rental_app/ui/states/pass_state.dart';
+import 'package:provider/provider.dart';
+
+List<InheritedProvider> get devProviders {
+  return [
+    Provider<PassRepository>(create: (_) => PassRepository()),
+    ChangeNotifierProvider<PassState>(
+      create: (context) => PassState(),
+    ),
+  ];
+}
 
 void main() {
-  mainCommon();
+  mainCommon(devProviders);
 }
